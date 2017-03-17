62f8c558ac7fc6ecb260440e9a012f81d73cd949
amitsng7 <sng.amit7@gmail.com>
2017-03-17 18:18:01 -0400

    Permutation

---
+++ Permutation of a number.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 17 18:16:43 2017
+
+@author: Amit
+"""
+
+def permutations(word):
+    if len(word)==1:
+        return [word] 
+    #get all permutations of length N-1
+    perms=permutations(word[1:])
+    char=word[0]
+    result=[]
+    #iterate over all permutations of length N-1
+    for perm in perms:
+        #insert the character into every possible location
+        for i in range(len(perm)+1):
+            result.append(perm[:i] + char + perm[i:])
+    return(result)
+permutations("abc")
